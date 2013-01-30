3650ba33a8745dcc992ef3f604269d9173d05a42
cdowen <hhylx1950@gmail.com>
2013-01-30 13:34:41 +0800

    deleted:    js/custom.js

--- js/custom.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-  $('form').submit(function(){
-    window.location.href="/index.php/start/search/"+encodeURIComponent($('#search-text').val())+'/'+encodeURIComponent($.trim($('.search-selected').html()))+'/1/';
-    return false;
-  });
-}); 
